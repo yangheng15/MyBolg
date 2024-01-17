92021ff8fd4c264d381e7cfa00c307d2d046d097
YangHeng <1220626014@qq.com>
2024-01-17 17:01:23 +0800

    feat: 雪花效果

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <Header />
-
+  <Sakura />
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from '@/components/Header/index.vue'
+import Sakura from '@/components/Sakura/index.vue'
 
 </script>
 
